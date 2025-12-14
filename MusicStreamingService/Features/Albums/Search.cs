ab98ef21b33dd39295d1cf56f0bb5c304a685d4c
Anton Nikittsev <38581029+Nhsdkk@users.noreply.github.com>
2025-12-14 15:38:50 +0300

    feat: add album search (#35)
    * feat: add album search
    
    * fix: resolve comments

---
+++ MusicStreamingService/Features/Albums/Search.cs
@@ -0,0 +1,165 @@
+using System.Text.Json.Serialization;
+using FluentValidation;
+using Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicStreamingService.Data;
+using MusicStreamingService.Data.Entities;
+using MusicStreamingService.Data.QueryExtensions;
+using MusicStreamingService.Data.Utils;
+using MusicStreamingService.Features.Users;
+using MusicStreamingService.Infrastructure.Authentication;
+using MusicStreamingService.Infrastructure.DateUtils;
+using MusicStreamingService.Infrastructure.ObjectStorage;
+using MusicStreamingService.Infrastructure.Result;
+using MusicStreamingService.Openapi;
+using MusicStreamingService.Requests;
+using MusicStreamingService.Responses;
+using MusicStreamingService.Validators;
+
+namespace MusicStreamingService.Features.Albums;
+
+[ApiController]
+public sealed class Search : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public Search(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Search albums by various filters
+    /// </summary>
+    /// <param name="request">Search filters</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpGet("/api/v1/albums/search")]
+    [Tags(RouteGroups.Albums)]
+    [Authorize(Roles = Permissions.ViewAlbumsPermission)]
+    [ProducesResponseType(typeof(QueryResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> SearchAlbums(
+        [FromQuery] Query request,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _mediator.Send(request, cancellationToken);
+        return Ok(result);
+    }
+
+    public sealed record Query : BasePaginatedRequest, IRequest<Result<QueryResponse>>
+    {
+        [JsonPropertyName("title")]
+        public string? Title { get; init; }
+
+        [JsonPropertyName("artistName")]
+        public string? ArtistName { get; init; }
+
+        [JsonPropertyName("releaseDateRange")]
+        public DateRange? ReleaseDateRange { get; init; }
+
+        public sealed class Validator : BasePaginatedRequestValidator<Query>
+        {
+            public Validator() : base()
+            {
+                RuleFor(x => x.Title)
+                    .NotEmpty()
+                    .MaximumLength(255)
+                    .When(x => x.Title is not null);
+
+                RuleFor(x => x.ArtistName)
+                    .NotEmpty()
+                    .MaximumLength(255)
+                    .When(x => x.ArtistName is not null);
+
+                RuleFor(x => x.ReleaseDateRange)
+                    .SetValidator(new DateRangeValidator()!)
+                    .When(x => x.ReleaseDateRange is not null);
+            }
+        }
+    }
+
+    public sealed record QueryResponse : BasePaginatedResponse
+    {
+        public sealed record ShortAlbumInfo
+        {
+            public Guid Id { get; init; }
+
+            public string Title { get; init; } = null!;
+
+            public long Likes { get; init; }
+
+            public ShortAlbumCreatorDto AlbumCreator { get; init; } = null!;
+
+            public string? ArtworkUrl { get; init; }
+
+            public static ShortAlbumInfo FromEntity(
+                AlbumEntity album,
+                string? artworkUrl) =>
+                new ShortAlbumInfo
+                {
+                    Id = album.Id,
+                    Title = album.Title,
+                    Likes = album.Likes,
+                    ArtworkUrl = artworkUrl,
+                    AlbumCreator = ShortAlbumCreatorDto.FromEntity(album.Artist)
+                };
+        }
+
+        public List<ShortAlbumInfo> Albums { get; init; } = new();
+
+        public static QueryResponse FromEntity(
+            int totalCount,
+            Query request,
+            List<AlbumEntity> albums,
+            Dictionary<string, string?> albumArtworkUrlMapping) =>
+            new QueryResponse
+            {
+                TotalCount = totalCount,
+                ItemsPerPage = request.ItemsPerPage,
+                ItemCount = albums.Count,
+                Page = request.Page,
+                Albums = albums
+                    .Select(x => ShortAlbumInfo.FromEntity(
+                        x,
+                        albumArtworkUrlMapping[x.S3ArtworkFilename]))
+                    .ToList()
+            };
+    }
+
+    public sealed class Handler : IRequestHandler<Query, Result<QueryResponse>>
+    {
+        private readonly MusicStreamingContext _context;
+        private readonly IAlbumStorageService _albumStorageService;
+
+        public Handler(
+            MusicStreamingContext context,
+            IAlbumStorageService albumStorageService)
+        {
+            _context = context;
+            _albumStorageService = albumStorageService;
+        }
+
+        public async ValueTask<Result<QueryResponse>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var albumsSearchQuery = _context.Albums
+                .AsNoTracking()
+                .FilterByOptionalAlbumCreator(request.ArtistName)
+                .FilterByOptionalReleaseDate(request.ReleaseDateRange)
+                .FilterByOptionalTitle(request.Title);
+
+            var totalCount = await albumsSearchQuery.CountAsync(cancellationToken);
+            var albums = await albumsSearchQuery
+                .Include(x => x.Artist)
+                .OrderByDescending(x => x.Likes)
+                .ApplyPagination(request.ItemsPerPage, request.Page)
+                .ToListAsync(cancellationToken);
+
+            var albumArtworkUrlsMapping =
+                await _albumStorageService.GetPresignedUrls(albums.Select(x => x.S3ArtworkFilename));
+
+            return QueryResponse.FromEntity(totalCount, request, albums, albumArtworkUrlsMapping);
+        }
+    }
+}
---
+++ MusicStreamingService/Validators/DateRangeValidator.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using MusicStreamingService.Data;
+using MusicStreamingService.Data.Utils;
+using MusicStreamingService.Infrastructure.DateUtils;
+
+namespace MusicStreamingService.Validators;
+
+public class DateRangeValidator : AbstractValidator<DateRange>
+{
+    public DateRangeValidator()
+    {
+        RuleFor(x => x)
+            .Must(x => x.Start <= x.End)
+            .When(x => x.Start is not null && x.End is not null)
+            .WithMessage("Start date must be less than or equal to end date.");
+
+        RuleFor(x => x.Start)
+            .NotEmpty()
+            .When(x => x.Start is not null);
+        
+        RuleFor(x => x.End)
+            .NotEmpty()
+            .When(x => x.End is not null);
+    }
+}
